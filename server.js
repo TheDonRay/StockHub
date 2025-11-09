2cf9f4be79760db9e066c993a1730bd65b553a9b
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-09 13:55:52 -0500

    setting up express app here

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express'); 
 const app = express();  
-require('dotenv').config(); 
+require('dotenv').config(); // for the dot env here.
 
 // simple middleare to get started. 
 app.use(express.json()); 
 
-const PORT = process.env.PORT; 
+const PORT = process.env.PORT;  
+
+app.get('/', (req, res) => { 
+    res.send({ 
+        ResponseMessage: 'Backend is running Ray'
+    }); 
+}); 
+
+app.listen(PORT, () => { 
+    console.log(`Server running on http://localhost:${PORT}`); 
+}); 
+
